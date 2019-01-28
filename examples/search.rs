e864421348699fefd8ad44fc4a081898c93c856d
Lenard Pratt <l3np27@gmail.com>
2019-01-28 19:01:49 +0000

    tidy example

--- examples/search.rs
+++ examples/search.rs
@@ -6,8 +6,7 @@ use rusty_royale::Royale;
 use rusty_royale::{Clan, ClanSearch, ClanTag, SearchResponse};
 
 fn main() {
-    let query = ClanSearch::new("boom baby");
-
+    
     let mut royale = Royale::new("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImU0YzdlM2ViLTQzOTUtNDVkMi1iMmRiLTM0NGE2ODZlMzkyNSIsImlhdCI6MTU0ODY3OTA0Miwic3ViIjoiZGV2ZWxvcGVyL2NmZGEwNDgwLWRiNjItMTk2Yi1iNjlmLWVlNzg5MWU1Y2I3YyIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI1LjcxLjE3OS41NSIsIjE2MS4yMy41Ni4yMTciXSwidHlwZSI6ImNsaWVudCJ9XX0.rOydgmNrD8UatM5gu9BkPEyNaQYdGd-46ijlxtOjw9vkkofafJcfYnMnQatwsg2IHmeOFlCKhWYLFiKeJb6NDQ").unwrap();
 
     let resp = royale
